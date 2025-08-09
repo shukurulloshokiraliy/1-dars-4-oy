64f3655f83f9770827ba2ac23dc4f1b655dbc744
Shukurullo <shukurulloshokiraliyev699@gmail.com>
2025-08-09 09:20:25 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,143 @@
+// 1-misol
+
+// export function showAge(age){
+//     console.log(`Sizning yoshingiz: ${age}`);
+
+// }
+
+// export const showAge2=function(age){
+//     console.log(`Sizning yoshingiz: ${age}`);
+// }
+
+// export const showAge3=(age)=>{
+//     console.log(`Sizning yoshingiz: ${age}`);
+// }
+
+// 2-misol
+
+// export function addNumbers(a,b){
+//     console.log(a+b);
+// }
+
+// export const addNumbers2 = function (a, b) {
+//   console.log(a + b);
+// };
+
+// export const addNumbers3 = (a, b) => {
+//   console.log(a + b);
+// }
+
+ // 3-misol
+
+// export function greet(name) {
+//   console.log(`Salom, ${name}`);
+// }
+
+// export const greet2 = function (name) {
+//   console.log(`Salom, ${name}`);
+// }
+
+// export const greet3 = (name) => {
+//   console.log(`Salom, ${name}`);
+// };
+
+// 4-misol
+
+// export function fullName(firstName, lastName){
+//     console.log(`${firstName} ${lastName}`)
+// }
+
+// export const fullName2=function(firstName, lastName){
+//     console.log(`${firstName} ${lastName}`)
+// }
+
+// export const fullName3=(firstName, lastName)=>{
+//     console.log(`${firstName} ${lastName})`
+// }
+
+// 5-misol
+
+// export function rectanglePerimeter(length, width){
+//     console.log(`${(length+width)*2}`)
+// }
+
+// export const rectanglePerimeter2=function(length, width){
+//     console.log(`${(length+width)*2}`)
+// }
+
+// export const rectanglePerimeter3=(length, width)=>{
+//     console.log(`${(length+width)*2})`
+// }
+
+
+// 6-masala
+// export function triangleSides1(a, b, c) {
+//     console.log("Tomon a:", a);
+//     console.log("Tomon b:", b);
+//     console.log("Tomon c:", c);
+//   }
+  
+//   export const triangleSides2 = function (a, b, c) {
+//     console.log("Tomon a:", a);
+//     console.log("Tomon b:", b);
+//     console.log("Tomon c:", c);
+//   };
+  
+//   export const triangleSides3 = (a, b, c) => {
+//     console.log("Tomon a:", a);
+//     console.log("Tomon b:", b);
+//     console.log("Tomon c:", c);
+//   };
+  
+//   // 7-masala
+//   export function squareNumber1(number) {
+//     console.log(number * number);
+//   }
+  
+//   export const squareNumber2 = function (number) {
+//     console.log(number * number);
+//   };
+  
+//   export const squareNumber3 = (number) => {
+//     console.log(number * number);
+//   };
+  
+//   // 8-masala
+//   export function animalInfo1(name, age) {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+//   }
+  
+//   export const animalInfo2 = function (name, age) {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+//   };
+  
+//   export const animalInfo3 = (name, age) => {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+//   };
+  
+//   // 9-masala
+//   export function sayHello1() {
+//     console.log("Salom Dunyo!");
+//   }
+  
+//   export const sayHello2 = function () {
+//     console.log("Salom Dunyo!");
+//   };
+  
+//   export const sayHello3 = () => {
+//     console.log("Salom Dunyo!");
+//   };
+  
+//   // 10-masala
+//   export function printNames1(name1, name2, name3, name4, name5) {
+//     console.log(name1, name2, name3, name4, name5);
+//   }
+  
+//   export const printNames2 = function (name1, name2, name3, name4, name5) {
+//     console.log(name1, name2, name3, name4, name5);
+//   };
+  
+//   export const printNames3 = (name1, name2, name3, name4, name5) => {
+//     console.log(name1, name2, name3, name4, name5);
+//   };
+  
---
+++ js/function.js
@@ -0,0 +1,102 @@
+import {
+    // 1-masala
+    showAge,
+    showAge2,
+    showAge3,
+  
+    // 2-masala
+    addNumbers,
+    addNumbers2,
+    addNumbers3,
+  
+    // 3-masala
+    greet,
+    greet2,
+    greet3,
+  
+    // 4-masala
+    fullName,
+    fullName2,
+    fullName3,
+  
+    // 5-masala
+    rectanglePerimeter,
+    rectanglePerimeter2,
+    rectanglePerimeter3,
+  
+    // 6-masala
+    triangleSides1,
+    triangleSides2,
+    triangleSides3,
+  
+    // 7-masala
+    squareNumber1,
+    squareNumber2,
+    squareNumber3,
+  
+    // 8-masala
+    animalInfo1,
+    animalInfo2,
+    animalInfo3,
+  
+    // 9-masala
+    sayHello1,
+    sayHello2,
+    sayHello3,
+  
+    // 10-masala
+    printNames1,
+    printNames2,
+    printNames3,
+  } from "./function.js";
+  
+  // 1-masala
+  showAge(24);
+  showAge2(25);
+  showAge3(15);
+  
+  // 2-masala
+  addNumbers(3, 7);
+  addNumbers2(2, 2);
+  addNumbers3(3, 5);
+  
+  // 3-masala
+  greet("Ali");
+  greet2("Hasan");
+  greet3("Nasimhon");
+  
+  // 4-masala
+  fullName("Ali", "Hasanov");
+  fullName2("Qulpiniso", "Hasanova");
+  fullName3("Janona", "Jalilova");
+  
+  // 5-masala
+  rectanglePerimeter(3, 5);
+  rectanglePerimeter2(6, 8);
+  rectanglePerimeter3(2, 2);
+  
+  // 6-masala
+  triangleSides1(3, 4, 5);
+  triangleSides2(3, 4, 5);
+  triangleSides3(3, 4, 5);
+  
+  // 7-masala
+  squareNumber1(5);
+  squareNumber2(5);
+  squareNumber3(5);
+  
+  // 8-masala
+  animalInfo1("Mushuk", 2);
+  animalInfo2("Mushuk", 2);
+  animalInfo3("Mushuk", 2);
+  
+  // 9-masala
+  sayHello1();
+  sayHello2();
+  sayHello3();
+  
+  // 10-masala
+  printNames1("Ali", "Vali", "Gulbahor", "Zilola", "Javohir");
+  printNames2("Ali", "Vali", "Gulbahor", "Zilola", "Javohir");
+  printNames3("Ali", "Vali", "Gulbahor", "Zilola", "Javohir");
+  
